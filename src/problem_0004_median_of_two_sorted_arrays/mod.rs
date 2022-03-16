0a5223fdc6d14c7b1ff5d978a00cf871b1c4bd91
BORBER <30563826+Borber@users.noreply.github.com>
2022-03-16 11:00:03 +0800

    [add] problem_0004_median_of_two_sorted_arrays 完成

--- src/problem_0004_median_of_two_sorted_arrays/mod.rs
+++ src/problem_0004_median_of_two_sorted_arrays/mod.rs
@@ -11,7 +11,8 @@ mod tests {
     pub fn run<S:Solution>() {
         let test_case = [
             ((&[1,3] as &[_], &[2] as &[_]), 2.00000),
-            ((&[1,2], &[3,4]), 2.50000)
+            ((&[1,2], &[3,4]), 2.50000),
+            ((&[0,0], &[0,0]), 0.00000)
         ];
         for ((nums1, nums2), expected) in test_case {
             assert_eq!(S::find_median_sorted_arrays(nums1.to_vec(), nums2.to_vec()), expected);
--- src/problem_0004_median_of_two_sorted_arrays/simple.rs
+++ src/problem_0004_median_of_two_sorted_arrays/simple.rs
@@ -1,14 +1,69 @@
 pub struct Solution;
 
+use std::cmp::Ordering;
+
 impl Solution {
+
     pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
         let (nums1, nums2) = if nums1.len() > nums2.len() {(nums1, nums2)} else { (nums2, nums1) };
-        match (nums1.clone().iter().next(), nums2.clone().iter().next()) {
+        let mid = (nums1.len() + nums2.len()) / 2;
+        let jo = (nums1.len() + nums2.len()) % 2;
+        match (nums1.clone().first(), nums2.clone().first()) {
             (None, None) => 0f64,
-            (num1, None) => *num1.unwrap() as f64,
-            (None, num2) => *num2.unwrap() as f64,
+            (_, None) => {
+                if jo == 0 {
+                    (nums1[mid - 1] + nums1[mid]) as f64 / 2 as f64
+                } else {
+                    nums1[mid] as f64
+                }
+            }
+            (None, _) => {
+                if jo == 0 {
+                    (nums2[mid - 1] + nums2[mid]) as f64 / 2 as f64
+                } else {
+                    nums2[mid] as f64
+                }
+            }
             (_, _) => {
-                0f64
+                let mut i1 = 0;
+                let mut i2 = 0;
+                let mut num1 = 0;
+                let mut num2 = 0;
+                loop{
+                    if i1 + i2 > mid {
+                        break
+                    }
+                    num2 = num1;
+                    if i1 == nums1.len() {
+                        num1 = nums2[i2];
+                        i2 += 1;
+                        continue
+                    }
+                    if i2 == nums2.len() {
+                        num1 = nums1[i1];
+                        i1 += 1;
+                        continue
+                    }
+                    match nums1[i1].cmp(&nums2[i2]){
+                        Ordering::Less => {
+                            num1 = nums1[i1];
+                            i1 += 1;
+                        }
+                        Ordering::Equal => {
+                            num1 = nums1[i1];
+                            i1 += 1;
+                        }
+                        Ordering::Greater => {
+                            num1 = nums2[i2];
+                            i2 += 1;
+                        }
+                    }
+                }
+                if jo == 0 {
+                    (num1 + num2) as f64 / 2 as f64
+                } else {
+                    num1 as f64
+                }
             }
         }
     }
